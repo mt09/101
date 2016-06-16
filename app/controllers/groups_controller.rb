2c7cd471ef1bc5be26ec4d9d0a2c34e73a531b3a
wen <sunzibinfa@gmail.com>
2016-06-16 18:18:40 +0800

    index, destroy action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -3,6 +3,16 @@ class GroupsController < ApplicationController
 		@groups = Group.all
 	end
 
+	def show
+		@group = Group.find(params[:id])
+	end
+
+	def destroy
+		@group = Group.find(params[:id])
+		@group.destroy
+		redirect_to groups_path
+	end
+
 	def new
 		@group = Group.new
 	end
