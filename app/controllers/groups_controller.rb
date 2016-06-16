625d702e25dfd44e85b50db103a9d4cfa5cee2d3
wen <sunzibinfa@gmail.com>
2016-06-16 18:03:33 +0800

    groups/edit, 用form.html.erb來做partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,20 @@ class GroupsController < ApplicationController
 		end
 	end
 
+	def edit
+		@group = Group.find(params[:id])
+	end
+
+	def update
+		@group = Group.find(params[:id])
+
+		if @group.update(group_params)
+			redirect_to groups_path
+		else
+			render :edit
+		end
+	end
+
 	private
 
 	def group_params
