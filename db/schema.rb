9be682d2aedee8b6bb9c92f2565aa26b6419cb80
wen <sunzibinfa@gmail.com>
2016-06-17 01:59:25 +0800

    add name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616175206) do
+ActiveRecord::Schema.define(version: 20160616175852) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "title"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20160616175206) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.boolean  "is_admin"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
