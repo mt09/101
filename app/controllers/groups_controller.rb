6dc3b9e30f61b385326a092f86f1642ca2db7518
wen <sunzibinfa@gmail.com>
2016-06-17 11:27:04 +0800

    group作者才能看見按鈕進行編輯與刪除,

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
 	end
 
 	def destroy
-		@group = Group.find(params[:id])
+		@group = current_user.groups.find(params[:id])
 		@group.destroy
 		redirect_to groups_path
 	end
@@ -21,7 +21,7 @@ class GroupsController < ApplicationController
 	end
 
 	def create
-		@group = Group.new(group_params)
+		@group = current_user.groups.new(group_params)
 
 		if @group.save
 			redirect_to groups_path, notice: "COOL"
@@ -31,11 +31,11 @@ class GroupsController < ApplicationController
 	end
 
 	def edit
-		@group = Group.find(params[:id])
+		@group = current_user.groups.find(params[:id])
 	end
 
 	def update
-		@group = Group.find(params[:id])
+		@group = current_user.groups.find(params[:id])
 
 		if @group.update(group_params)
 			redirect_to groups_path
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,9 @@
 class Group < ActiveRecord::Base
 	has_many :posts, dependent: :destroy
+
+	belongs_to :owner, class_name: "User", foreign_key: :user_id
+
+	def editable_by?(user)
+		user && user == owner
+	end
 end
