38111784fec32625ba4c414f9551b7e611420017
wen <sunzibinfa@gmail.com>
2016-06-17 11:44:31 +0800

    bug fix, 頁面判斷是否為post作者, if yes, 能看到按鈕並進行action

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,6 @@ class Post < ActiveRecord::Base
 	belongs_to :author, class_name: "User", foreign_key: :user_id
 
 	def editable_by?(user)
-		user == user && author
+		user && user == author
 	end
 end
